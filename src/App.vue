f30feb95b39300af9b86b4090f346a76cc4799e8
Milan G <dev@milangress.de>
2020-10-27 19:04:41 +0100

    make google id changeable through URL

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    #nav
+      // router-link(to='/') Home
+      // router-link(to='/gen/1MuXLBM_WGHm9vS_jhAtDVKHGDK9FJ171fpkqwBXJIKU') Gen
+    router-view
 </template>
 
 <style lang="stylus">
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Generator from '@/views/Generator'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/gen/:gid',
+    name: 'Generator',
+    component: Generator
   },
   {
     path: '/about',
