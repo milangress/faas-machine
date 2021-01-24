a29e07febc250527282456eddd8a39f4e069b124
Milan G <dev@milangress.de>
2021-01-25 00:14:42 +0100

    style home screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   div(class="home")
-    router-link(to='/gen/1MuXLBM_WGHm9vS_jhAtDVKHGDK9FJ171fpkqwBXJIKU') Gen
+    router-link.button(to='/gen/1MuXLBM_WGHm9vS_jhAtDVKHGDK9FJ171fpkqwBXJIKU') Make new Sentence…
 </template>
 
 <script>
@@ -12,3 +12,16 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+.button
+  display block
+  padding 0.5rem
+  font-size 2vw
+  border 3px solid blue
+  cursor pointer
+.button:hover
+  border 3px solid white
+  background blue
+  color white
+</style>
