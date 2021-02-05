8ac377688ed86d69b9ce0e3e79e94b19d3f79829
Milan G <dev@milangress.de>
2021-02-05 19:03:12 +0100

    upgrade to vue3

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Generator from '@/views/Generator'
-
-Vue.use(VueRouter)
 
 const routes = [
   {
@@ -26,9 +23,8 @@ const routes = [
   }
 ]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
